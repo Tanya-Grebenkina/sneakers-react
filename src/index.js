d704a4d5bd201dc0fc50dd171e80168575cf985f
Grebenkina <grebenkina.2490@gmail.com>
2022-12-17 23:38:19 +0100

    fixed path

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/sneakers-react'>
       <App />
     </Router>
   </React.StrictMode>
